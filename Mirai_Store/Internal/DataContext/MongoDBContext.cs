883c811ac12ddf3ad98971fc1c8a36d175ef5370
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-05-05 20:34:10 +0700

    fix lại collection

--- Mirai_Store/Internal/DataContext/MongoDBContext.cs
+++ Mirai_Store/Internal/DataContext/MongoDBContext.cs
@@ -17,21 +17,21 @@ namespace Mirai_Store.Internal.DataContext
 
         
         public IMongoCollection<User> User
-            => _database.GetCollection<User>("Users");
+            => _database.GetCollection<User>("users");
         public IMongoCollection<Game> Games
-            => _database.GetCollection<Game>("Games");
+            => _database.GetCollection<Game>("games");
         public IMongoCollection<Category> Categories
-            => _database.GetCollection<Category>("Categories");
+            => _database.GetCollection<Category>("categories");
         public IMongoCollection<Order> Orders
-            => _database.GetCollection<Order>("Orders");
+            => _database.GetCollection<Order>("orders");
         public IMongoCollection<OrderItem> OrderItems
-            => _database.GetCollection<OrderItem>("OrderItems");
+            => _database.GetCollection<OrderItem>("orderItems");
         public IMongoCollection<DiscountCode> DiscountCodes
-            => _database.GetCollection<DiscountCode>("DiscountCodes");
+            => _database.GetCollection<DiscountCode>("discountCodes");
         public IMongoCollection<Transaction> Transactions
-            => _database.GetCollection<Transaction>("Transactions");
+            => _database.GetCollection<Transaction>("transactions");
         public IMongoCollection<Wishlist> Wishlists
-            => _database.GetCollection<Wishlist>("Wishlists");
+            => _database.GetCollection<Wishlist>("wishlists");
 
 
         public async Task<bool> CheckConnectionAsync()
--- Mirai_Store/obj/Debug/net10.0/Mirai_Store.AssemblyInfo.cs
+++ Mirai_Store/obj/Debug/net10.0/Mirai_Store.AssemblyInfo.cs
@@ -7,13 +7,14 @@
 
 
 
+
 using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mirai_Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d196169cf8501c45c8b47704eef3b9462f05f94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf408618387a141ee8625cebd4fcbef1a413aaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mirai_Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mirai_Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
