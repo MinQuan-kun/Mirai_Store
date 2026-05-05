c958f95dab5353dfb32eadaba7f66c7104e87afa
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-05-05 18:52:51 +0700

    Đánh dấu thư mục

--- Mirai_Store/obj/Debug/net10.0/Mirai_Store.AssemblyInfo.cs
+++ Mirai_Store/obj/Debug/net10.0/Mirai_Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mirai_Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b3be3e2b87bc4ec55ebfcf901b7a6839528338")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d196169cf8501c45c8b47704eef3b9462f05f94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mirai_Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mirai_Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
