c982debb07d4160afdd401fe9b2d0cfe274271fc
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-05-05 18:32:46 +0700

    Controller đăng nhập

---
+++ Mirai_Store/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mirai_Store.Internal.Services.Interface;
+using Mirai_Store.Models.Auth;
+namespace Mirai_Store.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        
+        
+        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { Success = false, Message = "Dữ liệu không hợp lệ." });
+            }
+
+            
+            var result = await _authService.Register(request);
+
+            if (!result.Success)
+            {
+                return BadRequest(result); 
+            }
+
+            return Ok(result); 
+        }
+
+        
+        
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { Success = false, Message = "Dữ liệu không hợp lệ." });
+            }
+
+            
+            var result = await _authService.Login(request);
+
+            if (!result.Success)
+            {
+                return Unauthorized(result); 
+            }
+
+            
+            return Ok(result);
+        }
+    }
+}
